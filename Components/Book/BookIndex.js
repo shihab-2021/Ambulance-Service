c33eabddd98184ee2f2f0a6574f49c48960eced8
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-07-27 23:07:45 +0600

    update

--- Components/Book/BookIndex.js
+++ Components/Book/BookIndex.js
@@ -3,26 +3,55 @@ import DatePicker from "react-datepicker";
 import { FaCalendarAlt } from "react-icons/fa";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
+import { useForm } from "react-hook-form";
+import useAuth from "../Context/useAuth";
 
 const BookIndex = () => {
+  const { userInfo } = useAuth();
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm();
   const [startDate, setStartDate] = useState(new Date());
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const pickupDate = form.courseStartingDate?.value;
-    const caseName = form.case?.value;
-    console.log(caseName);
-    console.log(pickupDate);
-    const dateTime = new Date(pickupDate);
+  const [isLoading, setIsLoading] = useState(false);
 
-    // Get the local date and time strings
-    const localDateString = dateTime.toLocaleDateString();
-    const localTimeString = dateTime.toLocaleTimeString();
+  const submitHandler = async (info) => {
+    // event.preventDefault();
+    const rideInfo = {
+      ...info,
+      requester: userInfo,
+    };
+    console.log(rideInfo);
+    setIsLoading(true);
+    fetch("https://rescue-reach-server.vercel.app/rideRequest", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...rideInfo,
+      }),
+    }).then(() => {
+      alert("Ride request added!");
+      setIsLoading(false);
+    });
+    // const form = event.target;
+    // const pickupDate = form.courseStartingDate?.value;
+    // const caseName = form.case?.value;
+    // console.log(caseName);
+    // console.log(pickupDate);
+    // const dateTime = new Date(pickupDate);
 
-    // Combine the local date and time strings
-    const localDateTimeString = `${localDateString} ${localTimeString}`;
+    // // Get the local date and time strings
+    // const localDateString = dateTime.toLocaleDateString();
+    // const localTimeString = dateTime.toLocaleTimeString();
 
-    console.log(localDateTimeString);
+    // // Combine the local date and time strings
+    // const localDateTimeString = `${localDateString} ${localTimeString}`;
+
+    // console.log(localDateTimeString);
   };
   return (
     <div className="my-14">
@@ -31,7 +60,7 @@ const BookIndex = () => {
           <h1 className="text-xl font-bold text-black capitalize">
             Booking Details
           </h1>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit(submitHandler)}>
             <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
                 <label className="text-black" htmlFor="passwordConfirmation">
@@ -39,6 +68,7 @@ const BookIndex = () => {
                 </label>
                 <select
                   name="case"
+                  {...register("case")}
                   // {...register("location", { required: true })}
                   className="block w-full px-4 py-2 mt-2  rounded-md focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 >
@@ -54,6 +84,8 @@ const BookIndex = () => {
                   System
                 </label>
                 <select
+                  name="system"
+                  {...register("system")}
                   // {...register("location", { required: true })}
                   className="block w-full px-4 py-2 mt-2  rounded-md focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 >
@@ -68,16 +100,13 @@ const BookIndex = () => {
                   Pick Up Location
                 </label>
                 <input
+                  name="pickupLocation"
+                  {...register("pickupLocation")}
                   // {...register("productName", { required: true })}
                   id="pickupLocation"
                   type="text"
                   className="block w-full px-4 py-2 mt-2 rounded-md bg-gray-100 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
-                {/* {errors.productName && (
-                    <span className="mt-3 text-gray-800">
-                      productName field is required
-                    </span>
-                  )} */}
               </div>
 
               <div>
@@ -85,47 +114,60 @@ const BookIndex = () => {
                   Drop Location
                 </label>
                 <input
+                  name="dropLocation"
+                  {...register("dropLocation")}
                   // {...register("originalPrice", { required: true })}
                   id="dropLocation"
                   type="text"
                   className="block w-full px-4 py-2 mt-2 rounded-md bg-gray-100 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
-                {/* {errors.originalPrice && (
-                    <span className="mt-3 text-gray-800">
-                      originalPrice field is required
-                    </span>
-                  )} */}
               </div>
-            </div>
-            <div className="my-3">
-              <label className="text-black" htmlFor="date">
-                Date
-              </label>
-              <div className="relative">
+              <div>
+                <label className="text-black" htmlFor="cost">
+                  Cost(provable)
+                </label>
                 <input
-                  required=""
-                  // className="mt-6 ms-6 border rounded-md w-[307px] h-[50px] ps-2 text-[#535353] foc us:outline-0 bg-[#F6F7FF] "
-                  // className="py-2 border rounded-md w-full px-4"
+                  name="cost"
+                  {...register("cost")}
+                  // {...register("productName", { required: true })}
+                  id="cost"
+                  type="text"
                   className="block w-full px-4 py-2 mt-2 rounded-md bg-gray-100 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                  name="courseStartingDate"
-                  type="datetime-local"
-                  placeholder="Eg. Entrepreneurship Lab"
                 />
-                {/* <DatePicker
+              </div>
+              <div className="">
+                <label className="text-black" htmlFor="date">
+                  Date
+                </label>
+                <div className="relative">
+                  <input
+                    required=""
+                    {...register("rideDateTime")}
+                    // className="mt-6 ms-6 border rounded-md w-[307px] h-[50px] ps-2 text-[#535353] foc us:outline-0 bg-[#F6F7FF] "
+                    // className="py-2 border rounded-md w-full px-4"
+                    className="block w-full px-4 py-2 mt-2 rounded-md bg-gray-100 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                    name="rideDateTime"
+                    type="datetime-local"
+                    placeholder="Eg. Entrepreneurship Lab"
+                  />
+                  {/* <DatePicker
                   id="mydate"
                   className="py-2 border rounded-md w-full px-4"
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
                 />
                 <FaCalendarAlt className="absolute top-3 right-5" /> */}
+                </div>
               </div>
             </div>
 
-            <div>
+            <div className="my-3">
               <label className="text-black" htmlFor="desc">
                 Patient Description (Optional)
               </label>
               <textarea
+                name="description"
+                {...register("description")}
                 id="desc"
                 type="textarea"
                 className="block h-36 w-full px-4 py-2 mt-2 rounded-md bg-gray-100 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
